d864f2da0bed58f62be29e08630512d31da6add5
Aaron Mitchell <amitchell@cogolabs.com>
2016-11-22 23:16:46 -0500

    adding form fields to contact

--- src/views/contact.js
+++ src/views/contact.js
@@ -1,12 +1,21 @@
 import React, {Component} from 'react';
+
 export default class Contact extends Component {
   render() {
+
     return (
-      <div>
+      <div className="contact">
         <h2>
         Contact
         </h2>
         <p>Want to talk? Send me a message and let's get the conversation started.</p>
+        <label>Your Name</label>
+        <input placeholder="Your Name"></input>
+        <label>Your Email Address</label>
+        <input placeholder="Your Email"></input>
+        <label>Your Message</label>
+        <textarea placeholder="Your Message" rows="7"></textarea>
+        <button className="btn--contact" onclick={this.props.transporter.sendMail}>Send Message</button>
       </div>
     )
   }

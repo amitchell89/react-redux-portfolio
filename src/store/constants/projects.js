b81d7f54287a72c72c64ea9ef134c125b14fd229
Aaron Mitchell <amitchell@cogolabs.com>
2016-12-23 18:01:57 -0500

    adding arduino keyboard project

--- src/store/constants/projects.js
+++ src/store/constants/projects.js
@@ -17,7 +17,7 @@ export const projects = [
        git: false,
        git_link: "Work Project (Code Not Public)",
        about: "Link Ventures is a portfolio site for the investment firm that backs my Cogo Labs. I designed the brand and logo, created the wireframe and site layout, designed the mockup, contributed in building out the front end and handled all the styling and responsive work. It was built on a proprietary framework that utilized node, jade, stylus, jQuery and keystone JS for the CMS.",
-       hidden: false
+       hidden: true
     },
     {
        name: "Umbrella Check",
@@ -27,7 +27,7 @@ export const projects = [
        git: false,
        git_link: "Work Project (Code Not Public)",
        about: "Umbrella Check is a weather site I worked on at Cogo Labs. I created the wireframe and site layout, designed the mockup, contributed in building out the front end and handled all the styling and responsive work. The brand and logo were created by a coworker.",
-       hidden: false
+       hidden: true
     },
     {
        name: "Thrive Medicare",
@@ -37,7 +37,7 @@ export const projects = [
        git: false,
        git_link: "Work Project (Code Not Public)",
        about: "Thrive Medicare is a healthcare marketplace lead generation website I worked on at Cogo Labs, for one of our portfolio companies, Equate Analytics. I designed the brand and logo, created the wireframe and site layout, designed the mockup, worked with their lead developer in building out the front end and handled all the styling and responsive work. The site is built using Ruby on Rails, Html, Sass and jQuery.",
-       hidden: false
+       hidden: true
     },
     {
        name: "Cogo Labs 404 Page",
@@ -47,7 +47,7 @@ export const projects = [
        git: false,
        git_link: "Work Project (Code Not Public)",
        about: "I was tasked with creating a fun and interesting 404 easter egg page for my works main website. It features a digital illustration I made that has been layered to create a parallax effect based on the location of the mouse.",
-       hidden: false
+       hidden: true
     },
     {
        name: "Schillaci Guitars",
@@ -77,7 +77,7 @@ export const projects = [
        git: false,
        git_link: "Work Project (Code Not Public)",
        about: "Table Flipper is a node command line interface tool that I built to make coding emails easier and faster. It allows you to code emails using Jade for the markup, Sass for inline styles and Json for declaring variables and rendering options. The code is compiled into two versions, a clean production version, and a populated version for testing. It includes options to manipulate the final html by randomizing class names, shuffling html attributes and deleting unused classes.Table Flipper also provides a library of common partials for reusable elements and email fixes, and allows users to create blueprints, or starter templates for an even faster workflow. It utilizes nodemon for watching for changes and AWS syncing for quickly uploading images to Amazon S3.",
-       hidden: false
+       hidden: true
     },
     {
        name: "Offer Coffer",
@@ -87,6 +87,16 @@ export const projects = [
        git: false,
        git_link: "Work Project (Code Not Public)",
        about: "Offer Coffer is a React / Flux / Bootstrap internal tool that is used to manage, edit and review email offers and associated assets (subject lines, email templates etc). I was tasked with designing a new site layout and UI, adding react router, adding filtering to our main list view (along with URL query filtering), enabling multiple items to be saved simultaneously (even though they were still processed individually by the API), redesigning alerts and updating the UI to allow permissions to be set on an granular level for every asset within an offer.",
+       hidden: true
+    },
+    {
+      name: "Arduino Keyboard",
+       image: "projects/Aaron_Mitchell_Arduino_Keyboard.jpg",
+       url: false,
+       url_link: "Hardware Project",
+       git: true,
+       git_link: "https://github.com/amitchell89/arduino-keyboard",
+       about: "Using Arduino I built a tone generating keyboard. Its powered by a Mega Arduino 2560, which allows you more I/O than your standard arduino (54 pins). It features two octaves of Buttons and an up and down Octave Switch which moves the whole keyboard by one Octave. Its powered by battery and has both speaker and 1/4\" jacks so you can plug into your favorite amplifier.",
        hidden: false
     }
   ]

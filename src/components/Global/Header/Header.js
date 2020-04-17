8722078d691841b8e9d3b4a9cc110bb441542c60
Aaron Mitchell <aaronmitchellart@gmail.com>
2020-04-17 14:02:04 -0700

    cleaning up header

--- src/components/Global/Header/Header.js
+++ src/components/Global/Header/Header.js
@@ -7,25 +7,43 @@ export default class Header extends Component {
   render() {
     return (
       <header>
-        <div className="site_wrapper site_wrapper--main">
-          <div className="header__logo">
-            <Link to="/">
-              <h2>
-                Aaron Mitchell Design
-              </h2>
-            </Link>
+        <div className="header__container">
+          <div className="site_wrapper site_wrapper--main">
+
+
+            <div className="header__logo">
+              <Link to="/">
+                <h2 className="header__title--desktop">
+                  Aaron Mitchell Design
+                </h2>
+                <h3  className="header__title--mobile">
+                  Aaron Mitchell Design
+                </h3>
+              </Link>
+            </div>
+
+            <div className="header__nav--desktop">
+              <Link to="/">
+                Home
+              </Link>
+              <Link to="/gallery">
+                Gallery
+              </Link>
+              <Link to="/contact">
+                Contact
+              </Link>
+            </div>
+
+            <div className="header__nav--mobile">
+              <button 
+                className="button--icon"
+              >
+                <img src="https://blacksquare.nyc3.digitaloceanspaces.com/portfolio/icons/icon_hamburger.svg" />
+              </button>
+            </div>
           </div>
-          <div className="header__nav">
-            <Link to="/">
-              Home
-            </Link>
-            <Link to="/gallery">
-              Gallery
-            </Link>
-            <Link to="/contact">
-              Contact
-            </Link>
-          </div>
+
+
         </div>
       </header>
     )

c3626c62cce114f661ccd9441d1d019c710b72f5
Aaron Mitchell <aaronmitchellart@gmail.com>
2020-04-17 12:25:48 -0700

    updating design of the modals

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -35,10 +35,6 @@ class Modal extends Component {
     this.setState({info: false})
   }
 
-  toggleInfo = () => {
-    this.setState({info: !this.state.info})
-  }
-
   render() {
     const { selectedImage, selectedGallery, images } = this.props;
 
@@ -53,26 +49,20 @@ class Modal extends Component {
 
     const prevId = image.id - 1;
     const nextId = image.id + 1;
-    const bkgdImg = {
-      backgroundImage: 'url(' + image.url + ')',
-    }
 
-    let prevClass = 'btn--modal'
-    let nextClass = 'btn--modal'
+    let prevClass = 'button--icon modal__button__previous';
+    let nextClass = 'button--icon modal__button__next';
     let prevDisabled = false;
     let nextDisabled = false;
     if (prevId < 0) {
-      prevClass = prevClass + ' btn--modal--disabled'
+      prevClass = prevClass + ' button--icon--disabled'
       prevDisabled = true;
     }
     if (nextId >= images[selectedGallery].length) {
-      nextClass = nextClass + ' btn--modal--disabled'
+      nextClass = nextClass + ' button--icon--disabled'
       nextDisabled = true;
     }
-    let modal__info_classes = 'modal__info'
-    if (this.state.info == true) {
-      modal__info_classes = 'modal__info modal__info--show'
-    }
+
     let helmet_title = 'Aaron Mitchell: ' + image.name;
     let helmet_og_title = image.name + ' by Aaron Mitchell. ' + image.medium + '. ' + image.year + '.'
     let helmet_desc = helmet_og_title + ' ' + this.props.meta.meta_standard
@@ -89,19 +79,46 @@ class Modal extends Component {
           ] }
         />
         <div className="site_wrapper">
-          <div className={modal__info_classes}>
-            <p>{image.name}</p>
-            <p>{image.medium}</p>
-            <p>{image.year}</p>
-            <p>{image.about}</p>
+          <div className="modal__container">
+
+            <img className="modal__img" src={image.url} />
+
+            <div className="modal__sidebar">
+              <div className="modal__navigation">
+                
+                <button 
+                  className={prevClass}
+                  disabled={prevDisabled}
+                  onClick={this.props.switchImage.bind(this, selectedGallery, prevId)}
+                >
+                  <img src="https://blacksquare.nyc3.digitaloceanspaces.com/portfolio/icons/icon_previous.svg" />
+                </button>
+                
+                <button
+                  className={nextClass}
+                  disabled={nextDisabled}
+                  onClick={this.props.switchImage.bind(this, selectedGallery, nextId)} 
+                >
+                  <img src="https://blacksquare.nyc3.digitaloceanspaces.com/portfolio/icons/icon_next.svg" />
+                </button>
+
+                <button
+                  className="button--icon modal__button__close"
+                  onClick={this.props.closeModal.bind(this, selectedGallery)}
+                >
+                  <img src="https://blacksquare.nyc3.digitaloceanspaces.com/portfolio/icons/icon_close.svg" />
+                </button>
+
+              </div>
+
+              <div className="modal__info">
+                <h2>{image.name}</h2>
+                <p>{image.medium} &nbsp;|&nbsp; {image.year}</p>
+                <p>{image.about}</p>
+              </div>
+
+            </div>
           </div>
-          <div className="modal__buttons">
-            <button className="btn--modal btn--modal--info" onClick={this.toggleInfo}><i className="fa fa-info" aria-hidden="true"></i></button>
-            <button className={prevClass} disabled={prevDisabled} onClick={this.props.switchImage.bind(this, selectedGallery, prevId)}><i className="fa fa-chevron-left" aria-hidden="true"></i></button>
-            <button className={nextClass} disabled={nextDisabled} onClick={this.props.switchImage.bind(this, selectedGallery, nextId)}><i className="fa fa-chevron-right" aria-hidden="true"></i></button>
-            <button className="btn--modal btn--modal--close" onClick={this.props.closeModal.bind(this, selectedGallery)}><i className="fa fa-times" aria-hidden="true"></i></button>
-          </div>
-          <div className="modal__img" style={bkgdImg}></div>
         </div>
       </div>
     )

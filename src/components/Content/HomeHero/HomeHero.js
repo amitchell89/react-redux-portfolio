a35e3e1c19e4d60b4aaabbd0b58260b294c8de9e
Aaron Mitchell <aaronmitchellart@gmail.com>
2020-04-21 11:35:13 -0700

    adding content section to components, renaming about to HomeHero

--- src/components/Content/HomeHero/HomeHero.js
+++ src/components/Content/HomeHero/HomeHero.js
@@ -1,15 +1,15 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 
-export default class About extends Component {
+export default class HomeHero extends Component {
 
   render() {
     return (
-      <div className="About">
-        <div className="About--copy">
+      <div className="HomeHero">
+        <div className="HomeHero__copy">
           <h1 className="big-headline">Hello! My name is Aaron Mitchell, and I am a Designer living in Seattle, WA</h1>
         </div>
-        <div className="About--portrait"></div>
+        <div className="HomeHero__portrait"></div>
       </div>
     )
   }

a9f7b8b0edec8f8a1e837f9f7b56559f81954b4b
Aaron Mitchell <aaronmitchellart@gmail.com>
2020-04-23 12:33:45 -0700

    creating placeholder components for case studies

---
+++ src/components/Content/CaseStudy/RoverHomepageVision/RoverHomepageVision.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+
+export default class RoverHomepageVision extends Component {
+
+  render() {
+    return (
+      <div className="RoverHomepageVision">
+
+        <h1>Rover Homepage Vision</h1>
+
+      </div>
+    )
+  }
+}

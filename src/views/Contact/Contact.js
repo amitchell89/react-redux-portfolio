b8183b4e9f7eaf3fe9808d57817669974b3c5080
Aaron Mitchell <aaronmitchellart@gmail.com>
2020-04-28 00:55:46 -0700

    adding art to contact page

--- src/views/Contact/Contact.js
+++ src/views/Contact/Contact.js
@@ -29,19 +29,27 @@ export default class Contact extends Component {
         />
         <div className="site_wrapper site_wrapper--main">
           <div className="contact">
-            <h2>
-            Contact
-            </h2>
-            <p>Want to talk? Send me a message and let's get the conversation started.</p>
-            <form id="contact_form" method="post">
-              <label>Your Name</label>
-              <input type="text" name="name" placeholder="Your Name"></input>
-              <label>Your Email Address</label>
-              <input type="text" name="email" placeholder="Your Email"></input>
-              <label>Your Message</label>
-              <textarea type="text" name="message" placeholder="Your Message" rows="7"></textarea>
-              <button type="submit" form="contact_form" value="Submit" className="btn btn--contact">Send Message</button>
-            </form>
+
+            <div className="contact__form">
+              <h2>
+              Contact
+              </h2>
+              <p>Want to talk? Send me a message and let's get the conversation started.</p>
+              <form id="contact_form" method="post">
+                <label>Your Name</label>
+                <input type="text" name="name" placeholder="Your Name"></input>
+                <label>Your Email Address</label>
+                <input type="text" name="email" placeholder="Your Email"></input>
+                <label>Your Message</label>
+                <textarea type="text" name="message" placeholder="Your Message" rows="7"></textarea>
+                <button type="submit" form="contact_form" value="Submit" className="btn btn--contact">Send Message</button>
+              </form>
+            </div>
+
+            <div className="contact__art">
+              <img src="https://blacksquare.nyc3.digitaloceanspaces.com/portfolio/paintings/Aaron_Mitchell_Painting_What_Did_You_Say_700.jpg" />
+            </div>
+            
           </div>
         </div>
       </div>

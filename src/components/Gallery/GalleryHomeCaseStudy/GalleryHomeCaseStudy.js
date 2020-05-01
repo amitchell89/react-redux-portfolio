7e7b821f7862da6790802086f0ce1fee2531d907
Aaron Mitchell <aaronmitchellart@gmail.com>
2020-04-30 17:57:11 -0700

    adding lock icons to homepage case study links

--- src/components/Gallery/GalleryHomeCaseStudy/GalleryHomeCaseStudy.js
+++ src/components/Gallery/GalleryHomeCaseStudy/GalleryHomeCaseStudy.js
@@ -8,7 +8,8 @@ export default class GalleryHomeCaseStudy extends Component {
     return (
       <div className="GalleryHomeCaseStudy gallery__container gallery__container--full-width">
 
-        <div className="gallery__item">
+        <div className="gallery__item gallery__item__locked">
+          <div className="gallery__item__locked--icon"></div>
           <Link to="gallery/rover-homepage-vision">
             <div className="gallery__image gallery__image--rover-homepage-vision">
             </div>
@@ -18,7 +19,8 @@ export default class GalleryHomeCaseStudy extends Component {
           </Link>
         </div>
 
-        <div className="gallery__item">
+        <div className="gallery__item gallery__item__locked">
+          <div className="gallery__item__locked--icon"></div>
           <Link to="gallery/rover-growth-cro">
             <div className="gallery__image gallery__image--rover-cro">
             </div>

3b83dd68dd76b0c53f1c2e1ef0850dc9ca7923e2
Aaron Mitchell <aaronmitchellart@gmail.com>
2023-03-05 22:25:47 -0800

    debugging email issue, reverting to raw email for now

--- src/server_3000.js
+++ src/server_3000.js
@@ -10,6 +10,12 @@ var nodemailer = require('nodemailer');
 var helmet = require('helmet')
 var xss = require('xss');
 
+///////////////////
+// Configuration //
+//////////////////
+
+const site_name = 'Aaron Mitchell Design';  // This should come from meta_info file
+
 //////////////////
 // Helmet setup //
 //////////////////
@@ -21,9 +27,9 @@ app.use(bodyParser.json());
 
 // create reusable transporter object using the default SMTP transport
 // NodeMailer 0.7
-var password = process.env.PASSWORD || null
-var user = process.env.USER || null
-var service = process.env.SERVICE || null
+var password = process.env.EMAIL_PASSWORD || null
+var user = process.env.EMAIL_USER || null
+var service = process.env.EMAIL_SERVICE || null
 
 var transporter = nodemailer.createTransport("SMTP", {
   service: service,
@@ -60,31 +66,82 @@ app.use(authRoute);
 // Contact //
 ////////////
 
+// app.post('/contact', function(req, res) {
+//   var payload = req.body;
+//   var name = xss(payload.name)
+//   var email = xss(payload.email)
+//   var message = xss(payload.message)
+
+
+//   var email_message = '<b>From:</b> ' + name + '<br /><br /><b>Email:</b> ' + email + '<br /><br /><b>Message:</b> ' + message;
+
+//   // setup e-mail data with unicode symbols
+//   var mailOptions = {
+//     from: '"Aaron Mitchells Portfolio" <' + user + '>', // sender address
+//     to: 'aaronmitchellart@gmail.com', // list of receivers
+//     subject: 'New Message From Your Portfolio Site', // Subject line
+//     text: email_message, // plaintext body
+//     html: email_message // html body
+//   };
+
+//   // send mail with defined transport object
+//   transporter.sendMail(mailOptions, function(error, info){
+//     console.log('send mail')
+//     if(error){
+//         res.redirect('/post?post=fail')
+//         console.log('message failed to send')
+//         return console.log(error);
+//     }
+//     res.redirect('/post?post=success')
+//     console.log('message sent successfully')
+//   });
+//   transporter.close();
+// });
+
+
+
+
+////////////////////////////
+// Contact Page Endpoint //
+//////////////////////////
+
 app.post('/contact', function(req, res) {
   var payload = req.body;
-  var name = xss(payload.name)
-  var email = xss(payload.email)
-  var message = xss(payload.message)
-
-
+  var name = xss(payload.name);
+  var email = xss(payload.email);
+  var message = xss(payload.message);
   var email_message = '<b>From:</b> ' + name + '<br /><br /><b>Email:</b> ' + email + '<br /><br /><b>Message:</b> ' + message;
+  var subject = `${site_name} - New Message`;
+  var sendToAddress = 'aaronmitchellart@gmail.com'
+  sendMail(subject, email_message, sendToAddress, res);
+});
 
+////////////////////////////////
+// Use this to send an email //
+//////////////////////////////
+
+function sendMail(subject, message, sendToAddress, res) {
   // setup e-mail data with unicode symbols
   var mailOptions = {
-    from: '"Aaron Mitchells Portfolio" <' + user + '>', // sender address
-    to: 'aaronmitchellart@gmail.com', // list of receivers
-    subject: 'New Message From Your Portfolio Site', // Subject line
-    text: email_message, // plaintext body
-    html: email_message // html body
+    // from: `"${site_name}" <${user}>`, // sender address
+    from: '"Aaron Mitchell Design" <' + user + '>', // sender address
+    to: sendToAddress, // list of receivers
+    subject: subject, // Subject line
+    text: message, // plaintext body
+    html: message // html body
   };
-
   // send mail with defined transport object
   transporter.sendMail(mailOptions, function(error, info){
     if(error){
+        // res.end();
         res.redirect('/post?post=fail')
+        console.log('message failed to send')
         return console.log(error);
     }
+    // res.end();
     res.redirect('/post?post=success')
+    return console.log('Email sent successfully');
   });
   transporter.close();
-});
+}
+
